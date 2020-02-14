d88c34db87b1fc50c7281bab7a8d0ec7130877c0
jiteman <jiteman@gmail.com>
2020-02-14 09:37:24 +0300

    windows shared interface has been added

--- include/ogg/ogg.h
+++ include/ogg/ogg.h
@@ -23,6 +23,8 @@ extern "C" {
 #include <stddef.h>
 #include <ogg/os_types.h>
 
+#include "tpc_ogg.h"
+
 typedef struct {
   void *iov_base;
   size_t iov_len;
@@ -114,91 +116,160 @@ typedef struct {
 
 /* Ogg BITSTREAM PRIMITIVES: bitstream ************************/
 
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpack_writeinit(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int   oggpack_writecheck(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpack_writetrunc(oggpack_buffer *b,long bits);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpack_writealign(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpack_writecopy(oggpack_buffer *b,void *source,long bits);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpack_reset(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpack_writeclear(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpack_readinit(oggpack_buffer *b,unsigned char *buf,int bytes);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpack_write(oggpack_buffer *b,unsigned long value,int bits);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern long  oggpack_look(oggpack_buffer *b,int bits);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern long  oggpack_look1(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpack_adv(oggpack_buffer *b,int bits);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpack_adv1(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern long  oggpack_read(oggpack_buffer *b,int bits);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern long  oggpack_read1(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern long  oggpack_bytes(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern long  oggpack_bits(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern unsigned char *oggpack_get_buffer(oggpack_buffer *b);
 
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpackB_writeinit(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int   oggpackB_writecheck(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpackB_writetrunc(oggpack_buffer *b,long bits);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpackB_writealign(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpackB_writecopy(oggpack_buffer *b,void *source,long bits);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpackB_reset(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpackB_writeclear(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpackB_readinit(oggpack_buffer *b,unsigned char *buf,int bytes);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpackB_write(oggpack_buffer *b,unsigned long value,int bits);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern long  oggpackB_look(oggpack_buffer *b,int bits);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern long  oggpackB_look1(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpackB_adv(oggpack_buffer *b,int bits);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void  oggpackB_adv1(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern long  oggpackB_read(oggpack_buffer *b,int bits);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern long  oggpackB_read1(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern long  oggpackB_bytes(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern long  oggpackB_bits(oggpack_buffer *b);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern unsigned char *oggpackB_get_buffer(oggpack_buffer *b);
 
 /* Ogg BITSTREAM PRIMITIVES: encoding **************************/
 
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_stream_packetin(ogg_stream_state *os, ogg_packet *op);
-extern int      ogg_stream_iovecin(ogg_stream_state *os, ogg_iovec_t *iov,
-                                   int count, long e_o_s, ogg_int64_t granulepos);
+JMSD_TPC_OGG_SHARED_INTERFACE
+extern int      ogg_stream_iovecin(ogg_stream_state *os, ogg_iovec_t *iov, int count, long e_o_s, ogg_int64_t granulepos);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_stream_pageout(ogg_stream_state *os, ogg_page *og);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_stream_pageout_fill(ogg_stream_state *os, ogg_page *og, int nfill);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_stream_flush(ogg_stream_state *os, ogg_page *og);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_stream_flush_fill(ogg_stream_state *os, ogg_page *og, int nfill);
 
 /* Ogg BITSTREAM PRIMITIVES: decoding **************************/
 
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_sync_init(ogg_sync_state *oy);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_sync_clear(ogg_sync_state *oy);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_sync_reset(ogg_sync_state *oy);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_sync_destroy(ogg_sync_state *oy);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_sync_check(ogg_sync_state *oy);
 
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern char    *ogg_sync_buffer(ogg_sync_state *oy, long size);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_sync_wrote(ogg_sync_state *oy, long bytes);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern long     ogg_sync_pageseek(ogg_sync_state *oy,ogg_page *og);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_sync_pageout(ogg_sync_state *oy, ogg_page *og);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_stream_pagein(ogg_stream_state *os, ogg_page *og);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_stream_packetout(ogg_stream_state *os,ogg_packet *op);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_stream_packetpeek(ogg_stream_state *os,ogg_packet *op);
 
 /* Ogg BITSTREAM PRIMITIVES: general ***************************/
-
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_stream_init(ogg_stream_state *os,int serialno);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_stream_clear(ogg_stream_state *os);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_stream_reset(ogg_stream_state *os);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_stream_reset_serialno(ogg_stream_state *os,int serialno);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_stream_destroy(ogg_stream_state *os);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_stream_check(ogg_stream_state *os);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_stream_eos(ogg_stream_state *os);
 
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void     ogg_page_checksum_set(ogg_page *og);
 
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_page_version(const ogg_page *og);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_page_continued(const ogg_page *og);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_page_bos(const ogg_page *og);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_page_eos(const ogg_page *og);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern ogg_int64_t  ogg_page_granulepos(const ogg_page *og);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_page_serialno(const ogg_page *og);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern long     ogg_page_pageno(const ogg_page *og);
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern int      ogg_page_packets(const ogg_page *og);
 
+JMSD_TPC_OGG_SHARED_INTERFACE
 extern void     ogg_packet_clear(ogg_packet *op);
 
 
